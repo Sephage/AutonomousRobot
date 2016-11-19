1bbdcac9df24e56f99fb1fa657ed554d495b83d8
Paul VALENTIN <pmc.valentin@gmail.com>
2016-11-19 16:00:17 +0100

    sommage de l'image avec filtre de canny

--- src/main.c
+++ src/main.c
@@ -8,19 +8,24 @@
 int main(int* argv, char** argc){
 	CvCapture* capture = cvCaptureFromCAM(0);
 	IplImage* image = 0;
+	IplImage* canny = 0;
+	IplImage* gray = 0;
 	int64_t* columnDatas;
 	int16_t i;
-	char* name = "What an awesome image";
+	char* name = "What an awesome pic";
 
 	puts("Initialization ...");
 	
 	//if(!captureFrame(image, capture)) exit(1);
 	image = cvQueryFrame(capture);
+	canny = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+	gray = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
 	if(!image){
 		puts("Image fail to load");
 		exit(1);
 	}
 	columnDatas = malloc(image->width * sizeof(int64_t));
+	cvNamedWindow(name, CV_WINDOW_AUTOSIZE);
 
 	puts("Initialization complete !!");
 
@@ -29,19 +34,29 @@ int main(int* argv, char** argc){
 	printf("image->width = %i\n", image->width);
 	printf("image->nChannels = %i\n", image->nChannels);
 
-	getSumColumnValues(image, columnDatas);
+	while(cvWaitKey(30) == -1){
+		image = cvQueryFrame(capture);
+		if(!image){
+			puts("Image fail to load");
+			exit(1);
+		}
+		cvCvtColor(image, gray, CV_RGB2GRAY);
+		cvCanny( image, canny, 70, 100, 3);
+		getSumColumnValues(canny, columnDatas);
 
-	for(i=0 ; i<image->height ; i++){
-		printf("%li ", columnDatas[i]);
+		for(i=0 ; i<image->height ; i++){
+			printf("%li ", columnDatas[i]);
+		}
+		puts("");
+
+
+		cvShowImage(name, canny);
 	}
-	puts("");
 
-	cvNamedWindow(name, CV_WINDOW_AUTOSIZE);
-
-	cvShowImage(name, image);
-	
-	while(cvWaitKey(30) == -1);
+	cvReleaseCapture(&capture);
 	cvReleaseImage(&image);
+	cvReleaseImage(&canny);
+	cvReleaseImage(&gray);
 	free(columnDatas);
 
 	return 0;
