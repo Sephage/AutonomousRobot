0eead144e1dfe66a78a220f950b25d029be4df7b
BrunoBob <tessierb95@gmail.com>
2017-05-16 19:36:57 +0200

    save update arduino code for roomba

--- src/arduino/controlRoomba/controlRoomba.ino
+++ src/arduino/controlRoomba/controlRoomba.ino
@@ -1,4 +1,5 @@
-
+const int lenghtMsgmax = 3;
+char msg[lenghtMsgmax];
 
 //############################     FONCTIONS CONNEXION  REVEIL ROOMBA       ##################################
 
@@ -41,14 +42,51 @@ void rDrive(uint16_t velocity, uint16_t radius){
       return;
 }
 
+void cmd(int id, int value){
+  
+}
+
 void setup(){
   Serial.begin(115200);
   Serial1.begin(115200);
+  
+  for (int i=0; i<lenghtMsgmax; i++) {
+    msg[i] = -1;
+  }
   
   rStart();
   rFull();
 }
 
 void loop(){
-
+  int value = 0;
+  int id = 0; // 1 = move - 2 = turn - 3 = stop
+  while (Serial.available() > 0){
+    int nbBytesRecus=0;
+    for (int i=0; i<lenghtMsgmax; i++) { // boucle pour lecture successive des caractères en attente jusqu'à épuisement de la chaine        
+      if (Serial.available() > 0) {
+        char readByte = Serial.read(); //
+        nbBytesRecus++;
+        msg[i] = readByte; //
+      }        
+    }
+    
+    if(nbBytesRecus=3){
+      if(msg[0]==){
+        id = 
+      }
+      else if(msg[0]==){
+        id = 
+      }
+      else if(msg[0]==){
+        id = 
+      }
+      if(msg[1]==43) { // présence du signe "+" positi en deuxième caractère
+        value = int(msg[2]);
+      }
+      else if(msg[1]==45) { // présence du signe "-" négatif en deuxième caractère 
+        value = - (int(msg[2]));
+      }
+    }
+  }
 }
