958bccf9ddcd353b67e8f4828560c4dd9fdbd466
BrunoBob <tessierb95@gmail.com>
2017-06-15 13:19:32 +0200

    Added new test

--- src/rasp/imageProcessing/imageUtility.c
+++ src/rasp/imageProcessing/imageUtility.c
@@ -41,7 +41,7 @@ IplImage* captureImage(){
     IplImage *gray = 0;
     CvCapture *capture;
 
-    capture = cvCaptureFromCAM(0); // 0 for local webcam, 1 for external webcam or -1 for any. Use 0 for ext webcam on rpi.
+    capture = cvCaptureFromCAM(1); // 0 for local webcam, 1 for external webcam or -1 for any. Use 0 for ext webcam on rpi.
     image = cvQueryFrame(capture);
     if(!image)
     {
